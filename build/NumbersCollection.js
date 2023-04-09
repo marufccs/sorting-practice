1ab3415a9a04823ed73e63a3dd67064c4c0b630f
unknown <marufmozumder7@gmail.com>
2023-04-09 04:00:35 +0200

    numbers-collection-done

---
+++ build/NumbersCollection.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NumbersCollection = void 0;
+const Sorting_1 = require("./Sorting");
+class NumbersCollection extends Sorting_1.Sorting {
+    constructor(data) {
+        super();
+        this.data = data;
+    }
+    get length() {
+        return this.data.length;
+    }
+    compare(leftIndex, rightIndex) {
+        return this.data[leftIndex] > this.data[rightIndex];
+    }
+    swap(leftIndex, rightIndex) {
+        const leftHand = this.data[leftIndex];
+        this.data[leftIndex] = this.data[rightIndex];
+        this.data[rightIndex] = leftHand;
+    }
+}
+exports.NumbersCollection = NumbersCollection;
